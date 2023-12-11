423c87a18c0f2bc6c9ea1eb2bf7544228d9b5e64
david emioma <ue22aac@herts.ac.uk>
2023-12-11 16:58:10 +0000

    Updated prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import { UserButton } from "@clerk/nextjs";
 
 const Map = dynamic(() => import("../components/Map"), {
   ssr: false,
@@ -6,8 +7,14 @@ const Map = dynamic(() => import("../components/Map"), {
 
 export default function Home() {
   return (
-    <div className="w-full h-full bg-white dark:bg-[#1e1e1e] p-6">
-      <main className="w-full h-full flex flex-col md:flex-row gap-6">
+    <div className="w-full h-full bg-white dark:bg-[#1e1e1e]">
+      <nav className="flex items-center h-12 px-6 border-b dark:border-[#363636] shadow-sm">
+        <div className="ml-auto">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </nav>
+
+      <main className="w-full h-full md:h-[calc(100%-48px)] flex flex-col md:flex-row gap-6 p-6">
         <div className="flex flex-1 flex-col gap-6">
           <div className="h-12">Filter</div>
 
